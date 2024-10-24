a78278985e7aa6e5579c824f836fb5a54584ec1b
Kim Dokja <duc123456456456@gmail.com>
2024-10-24 19:48:44 +0700

    deployv1

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import pickle
 from flask import Flask, request, jsonify
 import predictor
 from predictor import predict_text_recording, predict_recording
+# import testmail
 
 nltk.download('stopwords')
 nltk.download('punkt')
@@ -27,11 +28,11 @@ lemmatizer = WordNetLemmatizer()
 
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-# model_name = "VietAI/envit5-translation"
-# tokenizer = AutoTokenizer.from_pretrained(model_name)  
-# model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+model_name = "VietAI/envit5-translation"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
-# with open('rf.pkl', 'rb') as file: 
+# with open('rf.pkl', 'rb') as file:
 #     rf = pickle.load(file)
 
 app = Flask(__name__)
@@ -46,122 +47,122 @@ app = Flask(__name__)
 # GENERAL_SALUTATION = r'\b(dear|hello|Good|Greetings)(?:\W+\w+){0,6}?\W+(user|customer|seller|buyer|account holder)\b'
 
 
-def extract_feature(mail):
-  urls = findallurls(mail)
-  feature = [0] * (9)
-  i = 0
-  feature[i]= int(body_richness(mail))
-  i+=1
-  feature[i]= int(presenceGeneralSalutation(mail)==True)
-  i+=1
-  feature[i]= maliciousURL(urls)
-  i+=1
-  feature[i]= hexadecimalURL(urls)
-  i+=1
-  feature[i]= int(contains_prime_targets(mail)==True)
-  i+=1
-  feature[i]= int(contains_account(mail)==True)
-  i+=1
-  feature[i]= int(contains_suspended(mail)==True)
-  i+=1
-  feature[i] = int(contains_password(mail)==True)
-  i+=1
-  feature[i]= len(urls)
-  return feature
+# def extract_feature(mail):
+#     urls = findallurls(mail)
+#     feature = [0] * (9)
+#     i = 0
+#     feature[i]= int(body_richness(mail))
+#     i+=1
+#     feature[i]= int(presenceGeneralSalutation(mail)==True)
+#     i+=1
+#     feature[i]= maliciousURL(urls)
+#     i+=1
+#     feature[i]= hexadecimalURL(urls)
+#     i+=1
+#     feature[i]= int(contains_prime_targets(mail)==True)
+#     i+=1
+#     feature[i]= int(contains_account(mail)==True)
+#     i+=1
+#     feature[i]= int(contains_suspended(mail)==True)
+#     i+=1
+#     feature[i] = int(contains_password(mail)==True)
+#     i+=1
+#     feature[i]= len(urls)
+#     return feature
 
-def cleanBody(mail_body):
-        filtered = []
-        filtered_text = cleanpunc(mail_body)
-        word_tokens = word_tokenize(filtered_text)
-        for w in word_tokens:
-                if w not in stop_words and w.isalpha():
-                    filtered.append(w)
-        return filtered
+# def cleanBody(mail_body):
+#         filtered = []
+#         filtered_text = cleanpunc(mail_body)
+#         word_tokens = word_tokenize(filtered_text)
+#         for w in word_tokens:
+#                 if w not in stop_words and w.isalpha():
+#                     filtered.append(w)
+#         return filtered
 
-def body_richness(mail_body):
-    mail_body = cleanBody(mail_body)
-    if len(set(mail_body))!=0:
-        return (len(mail_body)/len(set(mail_body)))
-    else:
-        return len(mail_body)
+# def body_richness(mail_body):
+#     mail_body = cleanBody(mail_body)
+#     if len(set(mail_body))!=0:
+#         return (len(mail_body)/len(set(mail_body)))
+#     else:
+#         return len(mail_body)
 
-def presenceGeneralSalutation(message):
-    return int(re.compile(GENERAL_SALUTATION,re.IGNORECASE).search(message) != None) == True
+# def presenceGeneralSalutation(message):
+#     return int(re.compile(GENERAL_SALUTATION,re.IGNORECASE).search(message) != None) == True
 
-def maliciousURL(urls):
-    count = 0
-    for url in urls:
-        if ((re.compile(IPREGEX, re.IGNORECASE).search(url)
-            is not None) == True or (len(re.compile(r'(https?://)',re.IGNORECASE).findall(url)) > 1)
-                or (len(re.compile(r'(www.)',re.IGNORECASE).findall(url)) > 1)
-                or (len(re.compile(r'(\.com|\.org|\.co)',re.IGNORECASE).findall(url)) > 1))== True:
-            count += 1
-    return count
+# def maliciousURL(urls):
+#     count = 0
+#     for url in urls:
+#         if ((re.compile(IPREGEX, re.IGNORECASE).search(url)
+#             is not None) == True or (len(re.compile(r'(https?://)',re.IGNORECASE).findall(url)) > 1)
+#                 or (len(re.compile(r'(www.)',re.IGNORECASE).findall(url)) > 1)
+#                 or (len(re.compile(r'(\.com|\.org|\.co)',re.IGNORECASE).findall(url)) > 1))== True:
+#             count += 1
+#     return count
 
-def cleanpunc(sentence): #function to clean the word of any punctuation or special characters
-    cleaned = re.sub(r'[?|!|\'|"|#]',r'',sentence)
-    cleaned = re.sub(r'[.|,|)|(|\|/]',r'',cleaned)
-    return  cleaned
+# def cleanpunc(sentence): #function to clean the word of any punctuation or special characters
+#     cleaned = re.sub(r'[?|!|\'|"|#]',r'',sentence)
+#     cleaned = re.sub(r'[.|,|)|(|\|/]',r'',cleaned)
+#     return  cleaned
 
-def hexadecimalURL(urls):
-    count = 0
-    for url in urls:
-        if ((re.compile(r'%[0-9a-fA-F]+', re.IGNORECASE).search(url)
-             is not None) == True):
-            count += 1
-    return count
+# def hexadecimalURL(urls):
+#     count = 0
+#     for url in urls:
+#         if ((re.compile(r'%[0-9a-fA-F]+', re.IGNORECASE).search(url)
+#             is not None) == True):
+#             count += 1
+#     return count
 
-def findallurls (content):
-  regex = r"(https?://\S+)"
+# def findallurls (content):
+#     regex = r"(https?://\S+)"
 
-  urls = re.findall(regex, content)
-  return urls
+#     urls = re.findall(regex, content)
+#     return urls
 
-def contains_prime_targets(subject):
-     subject = purify(subject)
-     jaro = textdistance.Jaro()
-     for w in subject.split():
+# def contains_prime_targets(subject):
+#     subject = purify(subject)
+#     jaro = textdistance.Jaro()
+#     for w in subject.split():
 
-         if ((jaro('bank',w)) >0.9 or (jaro('Paypal',w)) >0.9 or (jaro('ebay',w)) >0.9 or (jaro('amazon',w)) >0.9):
-            return 1
-     return 0
+#         if ((jaro('bank',w)) >0.9 or (jaro('Paypal',w)) >0.9 or (jaro('ebay',w)) >0.9 or (jaro('amazon',w)) >0.9):
+#             return 1
+#     return 0
 
-def contains_account(subject):
-     subject = purify(subject)
-     jaro = textdistance.Jaro()
-     for w in subject.split():
+# def contains_account(subject):
+#     subject = purify(subject)
+#     jaro = textdistance.Jaro()
+#     for w in subject.split():
 
-         if (jaro('account',w)) >0.9 or jaro('profile',w) >0.9 or jaro('handle',w) >0.9 :
-            return 1
-     return 0
+#         if (jaro('account',w)) >0.9 or jaro('profile',w) >0.9 or jaro('handle',w) >0.9 :
+#             return 1
+#     return 0
 
-def contains_suspended(subject):
-     subject = purify(subject)
-     jaro = textdistance.Jaro()
-     for w in subject.split():
+# def contains_suspended(subject):
+#     subject = purify(subject)
+#     jaro = textdistance.Jaro()
+#     for w in subject.split():
 
-         if (((jaro('closed',w)) or jaro('expiration',w))or jaro('suspended',w)) >0.9 or jaro('terminate',w) >0.9 or jaro('restricted',w) >0.9:
-            return 1
-     return 0
+#         if (((jaro('closed',w)) or jaro('expiration',w))or jaro('suspended',w)) >0.9 or jaro('terminate',w) >0.9 or jaro('restricted',w) >0.9:
+#             return 1
+#     return 0
 
-def contains_password(subject):
-     subject = purify(subject)
-     jaro = textdistance.Jaro()
-     for w in subject.split():
+# def contains_password(subject):
+#     subject = purify(subject)
+#     jaro = textdistance.Jaro()
+#     for w in subject.split():
 
-         if (jaro('password',w)) >0.9 or jaro('credential',w) > 0.9:
-            return 1
-     return 0
+#         if (jaro('password',w)) >0.9 or jaro('credential',w) > 0.9:
+#             return 1
+#     return 0
 
-def purify(subject):
-    filtered = ""
-    word_tokens = word_tokenize(subject)
-    for w in word_tokens:
-         if w not in stop_words and w.isalpha():
-                   w = stemmer.stem(w)
-                   filtered+=(lemmatizer.lemmatize(w))
-                   filtered+=" "
-    return filtered
+# def purify(subject):
+#     filtered = ""
+#     word_tokens = word_tokenize(subject)
+#     for w in word_tokens:
+#         if w not in stop_words and w.isalpha():
+#                 w = stemmer.stem(w)
+#                 filtered+=(lemmatizer.lemmatize(w))
+#                 filtered+=" "
+#     return filtered
 
 @app.route('/')
 def test_truecaller():
@@ -257,60 +258,68 @@ def predict_audio():
 
 @app.route('/service.html', methods=["POST", "GET"])
 def service():
-    if request.method == "POST":
-        try:
-            phone = request.form.get("phone_number")  # Dùng .get để tránh KeyError nếu không có giá trị
-            if phone:
-                country_code = "VN"
-                installation_id = "a1i0I--jMM3uXFb-ofc-ODmqyAGq8gHtLFxVeOdmifPv9kJWNeNABir5r72aykMM"
+    # if request.method == "POST":
+    #     try:
+    #         phone = request.form.get("phone_number")  # Dùng .get để tránh KeyError nếu không có giá trị
+    #         if phone:
+    #             country_code = "VN"
+    #             installation_id = "a1i0I--jMM3uXFb-ofc-ODmqyAGq8gHtLFxVeOdmifPv9kJWNeNABir5r72aykMM"
 
-                response = asyncio.run(search_phonenumber(phone, country_code, installation_id))
-                try:
-                    name_value = response['data']['data'][0]['name']
-                    if name_value:
-                        print(name_value)
-                except KeyError:
-                    name_value = "Đây là SĐT Bình thường"
-                return render_template('service.html', result1=name_value)
-            else:
-                # Nếu số điện thoại trống thì xử lý email
-                mail = request.form.get("email")  # Kiểm tra email
-                if not mail:
-                    return render_template('service.html', error="Vui lòng nhập số điện thoại hoặc email")
+    #             response = asyncio.run(search_phonenumber(phone, country_code, installation_id))
+    #             try:
+    #                 name_value = response['data']['data'][0]['name']
+    #                 if name_value:
+    #                     print(name_value)
+    #             except KeyError:
+    #                 name_value = "Đây là SĐT Bình thường"
+    #             return render_template('service.html', result1=name_value)
+    #         else:
+    #             # Nếu số điện thoại trống thì xử lý email
+    #             mail = request.form.get("email")  # Kiểm tra email
+    #             if not mail:
+    #                 return render_template('service.html', error="Vui lòng nhập số điện thoại hoặc email")
 
-                print(mail)
+    #             print(mail)
                 
-                detected_lang = detect(mail)
-                if detected_lang == "en":
-                    f_en = extract_feature(mail)
-                    f_en_np = np.array([f_en])
-                    predict = rf.predict(f_en_np)
-                    print(predict)
-                elif detected_lang == "vi":
-                    inputs = ['vi: ' + mail]
-                    outputs = model.generate(tokenizer(inputs, return_tensors="pt", padding=True).input_ids.to('cpu'), max_length=512)
-                    output = tokenizer.batch_decode(outputs, skip_special_tokens=True)
-                    output = output[0]
-                    f_vi = extract_feature(output[4:])
-                    f_vi_np = np.array([f_vi])
-                    predict = rf.predict(f_vi_np)
-                    print(predict)
-                else:
-                    predict = [-1]
+    #             detected_lang = detect(mail)
+    #             if detected_lang == "en":
+    #                 f_en = extract_feature(mail)
+    #                 f_en_np = np.array([f_en])
+    #                 predict = rf.predict(f_en_np)
+    #                 print(predict)
+    #             elif detected_lang == "vi":
+    #                 inputs = ['vi: ' + mail]
+    #                 outputs = model.generate(tokenizer(inputs, return_tensors="pt", padding=True).input_ids.to('cpu'), max_length=512)
+    #                 output = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    #                 output = output[0]
+    #                 f_vi = extract_feature(output[4:])
+    #                 f_vi_np = np.array([f_vi])
+    #                 predict = rf.predict(f_vi_np)
+    #                 print(predict)
+    #             else:
+    #                 predict = [-1]
                 
-                if predict[0] == 1: 
-                    result = "Đây là Email Lừa đảo"
-                elif predict[0] == 0: 
-                    result = "Đây là Email Bình thường"
-                else:
-                    result = "Ngôn ngữ không hợp lệ"
+    #             if predict[0] == 1:
+    #                 result = "Đây là Email Lừa đảo"
+    #             elif predict[0] == 0:
+    #                 result = "Đây là Email Bình thường"
+    #             else:
+    #                 result = "Ngôn ngữ không hợp lệ"
                 
-                return render_template('service.html', result2=result)
-        except Exception as e:
-            print(f"Lỗi: {e}")
-            return render_template('service.html', error="Đã xảy ra lỗi khi xử lý yêu cầu")
-    else:
+    #             return render_template('service.html', result2=result)
+    #     except Exception as e:
+    #         print(f"Lỗi: {e}")
+    #         return render_template('service.html', error="Đã xảy ra lỗi khi xử lý yêu cầu")
+    # else:
         return render_template('service.html')
+    
+@app.route('/testemail.html')
+def test_email():
+    # if request.method == 'POST':
+    #     email_text = request.form['emailText']
+    #     result = testmail.process_email(email_text)
+    #     return render_template('testemail.html', result2=result)
+    return render_template('testemail.html')
 
 
 @app.route('/install.html')
